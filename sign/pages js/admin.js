f7af4247a0b76b5f316e5238cae62088c6d1d37e
Devesh.Suryawanshi <deveshsuryawashi940@gmail.com>
2023-02-24 21:44:34 +0530

    day-4 CW Project

--- sign/pages js/admin.js
+++ sign/pages js/admin.js
@@ -34,14 +34,14 @@ Login_Btn.addEventListener("click", ()=>{
 })
 
 function login(obj){
+    console.log(obj);
     fetch(`http://localhost:3000/admins`)
     .then((res)=> res.json())
     .then((data)=>{
-        let count = 0;
         data.forEach((e) => {
-            if(e.email == obj.email && e.password == obj.password && e.admin == obj.admin){
-                display();
+            if(e.email == obj.email && e.password == obj.password && e.username == obj.admin){
                 alert("login Sucessfully");
+                Window.open(location.href = "/admin/admin.html");
             }else{
                 count++;
             }
@@ -53,14 +53,4 @@ function login(obj){
     .catch((error)=>{
         console.log(error);
     })
-}
-
-function display(){
-        loginSucessfully.style.border = "none";
-        loginSucessfully.innerHTML = `<div class="child-1" style="background-color: black; color:white; margin: auto; border-radius: 10px;">
-                                        <p>Login Sucessfully</p>
-                                      </div>`;
-        clickHereMassageBtn.style.border = "none";                              
-        clickHereMassageBtn.innerHTML = `<button style="background-color: #ee5456; color:white;"><a href="./register.html" style="text-decoration: none;color: white;">Click here to go Home page</a></button>`  
-        card.style="background-color: #e6e7e8"                        
 }
