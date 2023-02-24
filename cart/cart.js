cc0a1676a1598cacc440ee6716a39d8a646e4283
akash wakade <mr.akashwakade@gmail.com>
2023-02-24 20:56:38 +0530

    added day4 work

--- cart/cart.js
+++ cart/cart.js
@@ -1,3 +1,11 @@
 function gotoHome(){
-   window.Location.assign("https://www.w3schools.com")
+  window.location.assign('http://127.0.0.1:5501/index.html' )
+   
 }
+let login = document.getElementById("login")
+login.addEventListener('click',function(){
+  window.location.assign('http://127.0.0.1:5501/sign/pages%20js/login.html')
+})
+
+//location for register /sign/pages%20js/register.html
+//locatio for login /sign/pages%20js/login.html
