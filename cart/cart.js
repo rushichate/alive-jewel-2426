b2caccc85e25ea779e41850683cb95c52379007a
akash wakade <mr.akashwakade@gmail.com>
2023-02-25 22:00:18 +0530

    added day-5 work

--- cart/cart.js
+++ cart/cart.js
@@ -1,7 +1,11 @@
-function gotoHome(){
+let cbtn=document.getElementById("cbtn")
+cbtn.addEventListener('click',function(){
+  
   window.location.assign('http://127.0.0.1:5501/index.html' )
+})
+//   
    
-}
+// }
 let login = document.getElementById("login")
 login.addEventListener('click',function(){
   window.location.assign('http://127.0.0.1:5501/sign/pages%20js/login.html')
