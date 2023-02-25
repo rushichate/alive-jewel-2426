8a28716c625dcde50f744925a9638259a8ff4ee8
akash wakade <mr.akashwakade@gmail.com>
2023-02-25 22:35:00 +0530

    added

--- cart/cart.js
+++ cart/cart.js
@@ -1,7 +1,7 @@
 let cbtn=document.getElementById("cbtn")
 cbtn.addEventListener('click',function(){
   
-  window.location.assign('http://127.0.0.1:5501/index.html' )
+  window.location.assign('/payment/payment.html' )
 })
 //   
    
